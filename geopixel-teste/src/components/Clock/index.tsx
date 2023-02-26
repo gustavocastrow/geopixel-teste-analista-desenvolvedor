43aac5149a5769217fca91baad7c06ab446d457b
Gustavo Castro <gustavocastrocs@gmail.com>
2023-02-27 00:22:46 +0100

    feat/doc-styles-clock

--- geopixel-teste/src/components/Clock/index.tsx
+++ geopixel-teste/src/components/Clock/index.tsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from "react";
+import styles from './Clock.module.css';
 
 export function Clock(){
   const [time, setTime] = useState(new Date().toLocaleDateString());
 
+  //função getTime utilizada para pegar a data/hora atual
+  //e setar ela para uma string para ser renderizada na tela
   const getTime = () => {
     const date = new Date();
-    const currentTime = date.toLocaleTimeString();
-
+    console.log(date)
+    const currentTime = date.toLocaleTimeString('pt-BR');
+    
+    //Através do hook useState conseguimos fazer o gerenciamento do estado
+    //com a função setTime conseguimos alterar o estado do time.
     setTime(currentTime);
   }
 
+  //Aqui estamos utilizando o hook useEffect com a função setTimeout sendo passada
+  //como primeiro parametro que sera executada assim que o componente for renderizado,
+  //no segundo parametro temos um array com a variavel time que vai fazer
+  //a função setTimeout ser executada novamente.
   useEffect(() => { //hook de efeito colateral.
     setTimeout(getTime, 1000);
-  }, [time]); //array de dependencias
+  }, [time]);
 
   return (
-    <div>{time}</div>
+    <div className={styles.clockContainer}>
+      <h1>{time}</h1>
+    </div>
   )
 
 } 
