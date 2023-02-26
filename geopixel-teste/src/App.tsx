cad915d6571d8a073671043dcb24f7e25748092d
Gustavo Castro <gustavocastrocs@gmail.com>
2023-02-27 00:23:14 +0100

    feat/styles-global

--- geopixel-teste/src/App.tsx
+++ geopixel-teste/src/App.tsx
@@ -2,19 +2,32 @@ import { Clock } from './components/Clock';
 import { ItemHandler } from './components/ItemHandler';
 import { ShyBall } from './components/Shyball';
 
-export function App(){
-  return(
-    <div>
-      <Clock />
-      <ShyBall
-        stalker
-        ballHeight={50}
-        ballWidth={50}
-        boxHeight={400}
-        boxWidth={600}
-      />
-      <ItemHandler />
+import styles from './App.module.css';
+import './global.css';
 
+export function App(){
+  return (
+    <div>
+      <div className={styles.container}>
+        <div className={styles.section1}>
+          <div className={styles.clock}>
+            <Clock />
+          </div>
+          <div className={styles.handleForm}>
+            <ItemHandler />
+          </div>
+        </div>
+        <div className={styles.section2}>
+          <ShyBall
+            stalker={false} //caso stalker seja true uma bola branca ira seguir a bola vermelha
+            ballHeight={50}
+            ballWidth={50}
+            boxHeight={window.innerHeight}
+            boxWidth={window.innerWidth / 2}
+          />
+        </div>
+      </div>
+      
     </div>
   )
 }
